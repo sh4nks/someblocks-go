68adb94176dc7ec98697783c4ddbc4fb4d555275
Peter Justin <peter.justin@outlook.com>
2020-12-07 10:59:57 +0100

    Merge configs instead of overwriting them

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,11 +2,13 @@ package config
 
 import (
 	"bytes"
-	"encoding/json"
+	"crypto/rand"
+	"encoding/base64"
 
 	"github.com/mitchellh/mapstructure"
 	"github.com/rs/zerolog/log"
 	"github.com/spf13/viper"
+	yaml "gopkg.in/yaml.v2"
 )
 
 /*
@@ -20,12 +22,6 @@ map[string]interface{}{
     },
 })
 */
-
-func LoadConfig() {
-	loadConfigFromStruct(Cfg)
-	viper.AutomaticEnv()
-	viper.ReadInConfig()
-}
 
 type Database struct {
 	Driver   string `mapstructure:"driver"`
@@ -42,15 +38,18 @@ type Web struct {
 }
 
 type Config struct {
-	Database Database `mapstructure:"database"`
-	Web      Web      `mapstructure:"web"`
-	Logfile  string   `mapstructure:"logfile"`
+	Database  Database `mapstructure:"database"`
+	Web       Web      `mapstructure:"web"`
+	Logfile   string   `mapstructure:"logfile"`
+	SecretKey string   `mapstructure:"secretkey"`
+	Debug     bool     `mapstructure:"debug"`
 }
 
 var Cfg = &Config{
 	Database: Database{
 		Driver:   "sqlite3",
 		Dbname:   "someblocks.sqlite",
+		Host:     "",
 		Username: "",
 		Password: "",
 	},
@@ -58,51 +57,71 @@ var Cfg = &Config{
 		Host: "127.0.0.1",
 		Port: 8080,
 	},
-	Logfile: "someblocks.log",
+	Logfile:   "someblocks.log",
+	SecretKey: makeSecretKey(32),
+	Debug:     false,
 }
 
-func loadConfigFromStruct(cfg interface{}) {
+func loadDefaultConfig(cfg interface{}) {
 	cfgMap := make(map[string]interface{})
 	err := mapstructure.Decode(cfg, &cfgMap)
 	if err != nil {
 		log.Fatal().Err(err).Msg("failed to marshal default config")
 	}
 
-	cfgJsonBytes, err := json.Marshal(&cfgMap)
+	cfgYamlBytes, err := yaml.Marshal(&cfgMap)
 	if err != nil {
 		log.Fatal().Err(err).Msg("failed to marshal default config")
 	}
 
-	viper.SetConfigType("json")
-	err = viper.ReadConfig(bytes.NewReader(cfgJsonBytes))
+	viper.SetConfigType("yaml")
+	err = viper.ReadConfig(bytes.NewReader(cfgYamlBytes))
 	if err != nil {
 		log.Fatal().Err(err).Msg("failed to read default config")
 	}
 }
 
+func makeSecretKey(n int) string {
+	b := make([]byte, n)
+	_, err := rand.Read(b)
+	if err != nil {
+		log.Fatal().Err(err).Msg("Couldn't generate secret key.")
+	}
+	//return string(b[:])
+	return base64.URLEncoding.EncodeToString(b)
+}
+
 func Load(cfgFile string) {
-	viper.SetConfigName("config")          // name of config file (without extension)
-	viper.SetConfigType("yaml")            // REQUIRED if the config file does not have the extension in the name
-	viper.AddConfigPath(".")               // look for the default config in the src config directory
-	viper.AddConfigPath("/etc/someblocks") // look for config in the src config directory
+	loadDefaultConfig(Cfg)
+
+	viper.SetConfigName("config") // name of config file (without extension)
+	viper.AddConfigPath(".")      // look for the default config in the src config directory
 
 	if cfgFile != "" {
 		// Use config file from the flag.
 		viper.SetConfigFile(cfgFile)
 	}
 
-	err := viper.ReadInConfig() // Find and read the config file
-	if err != nil {             // Handle errors reading the config file
+	err := viper.MergeInConfig() // Find and read the config file
+	if err != nil {              // Handle errors reading the config file
 		log.Fatal().Msgf("Error using config file: %s", err)
 	}
-
 	viper.AutomaticEnv()
 
-	if err := viper.ReadInConfig(); err == nil {
+	if err := viper.MergeInConfig(); err == nil {
 		log.Info().Msgf("Using config file: %s", viper.ConfigFileUsed())
 	}
 
 	if err := viper.Unmarshal(Cfg); err != nil {
 		log.Fatal().Msgf("Couldn't unmarshal viper config into Cfg", err)
 	}
+}
+
+func ToYAML() string {
+	c := viper.AllSettings()
+	bs, err := yaml.Marshal(c)
+	if err != nil {
+		log.Fatal().Err(err).Msgf("unable to marshal config to YAML: %v", err)
+	}
+	return string(bs)
 }
