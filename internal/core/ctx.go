b665ce2ba8602203fd06fcb83483b81452775141
Peter Justin <peter.justin@outlook.com>
2020-12-14 21:46:47 +0100

    Add csrfField

--- internal/core/ctx.go
+++ internal/core/ctx.go
@@ -29,15 +29,11 @@ func (c *AppContext) Text(w http.ResponseWriter, status int, v string) {
 }
 
 func (c *AppContext) HTML(w http.ResponseWriter, r *http.Request, status int, tmpl string, data interface{}) {
-	csrfField := csrf.TemplateField(r)
 	htmlOpts := render.HTMLOptions{
 		Funcs: template.FuncMap{
 			"csrfField": func() template.HTML {
-				return csrfField
+				return csrf.TemplateField(r)
 			},
-			"testFunc": func() string {
-					return "My custom function"
-				},
 		},
 	}
 	c.Render.HTML(w, status, tmpl, data, htmlOpts)
